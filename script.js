8c99bb27766210650beb6fa8bda8c1c1e96b1b8c
Neelesh0124 <142391511+Neelesh0124@users.noreply.github.com>
2024-02-10 13:32:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
 // your code here
-}
+	 let a=0;
+	 let b=a+1;
+	 if (num==1) {
+		 return a;
+	 }
+	 else if (num==1) {
+		 return b;
+	 	
+	 }
+	else{
+		retun a+b;
+	}
+	
+	}
 
 module.exports = fibonacci;
